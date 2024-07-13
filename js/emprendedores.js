0cc6b99051b5e0d7b37f16b4a4de27bfa2ad60d6
Cam <camila.dambrosio95@gmail.com>
2024-07-13 03:30:30 -0300

    primer maquetado con api falopa de eventos, cambiar por api de personas random

---
+++ js/emprendedores.js
@@ -0,0 +1,109 @@
+
+let container = document.getElementById("container")
+let btnSearch = document.getElementById("btn_search")
+let inputSearch = document.getElementById("input_search")
+let searchForm = document.getElementById("searchForm")
+let events = []
+let allEvents = []
+
+async function cargarEventos() {
+    try {
+        const response = await axios.get("https://mindhub-xj03.onrender.com/api/amazing")
+        console.log(response.data.events)
+        events = response.data.events
+        allEvents = [...events]
+        dibujarEventos(events, container)
+    } catch (err) {
+        console.log("Error: " + err.message)
+    }
+}
+
+async function ejecutarDibujado() {
+    await cargarEventos()
+    createSearchListener(searchForm, events, container)
+    createInputListener(searchForm, events, container)
+    handleCategoryFilter(events)
+}
+
+
+ejecutarDibujado()
+
+
+let dibujarEventos = (array, container) => {
+    container.innerHTML = ""
+    
+    array.forEach((event) => {
+
+        if (event.assistance !== undefined) {
+            container.innerHTML += `
+                <div id="card" class="card" style="width: 18rem;">
+                    <img class="card_img" src="${event.image}">
+                    <h5 class="card-title"> ${event.name} </h5>
+                    <p class="card-text">${event.description}</p>
+                    <div class="card-body">
+                        <a id="price" href="#" class="card-link">Price: $${event.price}</a>
+                        <a href="./details.html" class="card-link">Details</a>
+                    </div>
+                </div>
+            `
+        }
+    })
+}
+
+//Creo un filtro para la barra de búsqueda
+let createSearchListener = (searchForm, events, container) => {
+    searchForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        let eventName = inputSearch.value
+    
+        let eventsFilter = events.filter(
+            (event)=>event.name.toLowerCase().includes(eventName.toLowerCase())
+        )
+    
+        if(eventsFilter.length===0){
+            dibujarEventos(events, container)
+        } else {
+            dibujarEventos(eventsFilter, container)
+        }
+    })
+}
+
+
+//a ese filtro le implemento la posibilidad de mostrar el contenido conforme se va tipeando en el input
+let createInputListener = (searchForm, events, container) => {
+    inputSearch.addEventListener("input", ()=> {
+        let eventName = inputSearch.value
+    
+        let eventsFilter = events.filter ((event) =>
+        event.name.toLowerCase().includes(eventName.toLowerCase()))
+        
+        if (eventName ==="") {
+            dibujarEventos(events, container)
+        } else {
+            dibujarEventos(eventsFilter, container)
+        }
+    })
+}
+
+function handleCategoryFilter(events) {
+    // Agrega un event listener para los checkboxes
+    const checkboxes = document.querySelectorAll(".category-filter")
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener("change", () => {
+            const selectedCategories = Array.from(checkboxes)
+                .filter((checkbox) => checkbox.checked)
+                .map((checkbox) => checkbox.value)
+
+            if (selectedCategories.length === 0) {
+                // Si no hay categorías seleccionadas, muestra todos los eventos sin filtrar
+                dibujarEventos(allEvents, container)
+            } else {
+                // Filtra los eventos por categoría
+                const eventsFilter = events.filter((event) => {
+                    return selectedCategories.includes(event.category)
+                })
+                dibujarEventos(eventsFilter, container)
+            }
+        })
+    })
+}
